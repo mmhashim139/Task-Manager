9ec3fe99694e6bc683f55da34ae0fdf916d15bfb
Mohamed Hashim <mmhashim139@gmail.com>
2020-10-28 17:17:58 +0000

    Add task form

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,16 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 

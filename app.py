fd186bee985b094104641d6814e171f42f2d9822
Mohamed Hashim <mmhashim139@gmail.com>
2020-10-28 02:45:43 +0000

    Registration Functionality

--- app.py
+++ app.py
@@ -28,6 +28,24 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check if username already exists in our DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exist")
+            return redirect(url_for('register'))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user info into session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Regestration Successful")
     return render_template("register.html")
 
 
